2c30e9f2f70087900add9c2556ed0dcc60f1d38c
HollyLgru <H.grudovich@zollege.com>
2023-03-06 15:04:24 -0600

    hmmm

--- src/App.js
+++ src/App.js
@@ -8,28 +8,32 @@ function App() {
   const options = {
     method: 'GET',
     url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI',
-    params: {q: 'taylor swift', pageNumber: '1', pageSize: '10', autoCorrect: 'true'},
+    params: {q: 'taylor swift', pageNumber: '1', pageSize: '2', autoCorrect: 'true'},
     headers: {
       'X-RapidAPI-Key': '977a5b648bmsh2c2dc55f0a331e2p127ba6jsnaa485c7ab02a',
       'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
     }
   };
 
+    const [data, setData] = useState([]);
+    
+    const getImages = async () => {
+      const { data } = await axios.request(options);
+      setImages(data);
+      console.log(data.value)
+    };
 
-  useEffect(() => {
-  axios.request(options).then(function (response) {
-    console.log(response.data.value);
-  }).catch(function (error) {
-    console.error(error);
-  });
+    useEffect(() => {
+      getImages();
     }, []);
+
+    console.log(images)
   
 
   return (
     <div className="App">
 
       <h1>Hello World</h1>
-
     </div>
   );
 }
