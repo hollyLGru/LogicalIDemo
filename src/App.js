acdfbf2dcc519ba3748c93113151a9511f0aca38
HollyLgru <H.grudovich@zollege.com>
2023-03-07 09:02:31 -0600

    stop using api call

--- src/App.js
+++ src/App.js
@@ -5,24 +5,24 @@ import  {React, useState, useEffect } from 'react';
 function App() {
   const [images, setImages] = useState([])
 
-  const options = {
-    method: 'GET',
-    url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI',
-    params: {q: 'ice cream', pageNumber: '1', pageSize: '2', autoCorrect: 'true'},
-    headers: {
-      'X-RapidAPI-Key': '1f337ed6e9mshbfacca2cfdfb894p120943jsn7edec953a7e2',
-      'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
-    }
-  };
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI',
+  //   params: {q: 'ice cream', pageNumber: '1', pageSize: '2', autoCorrect: 'true'},
+  //   headers: {
+  //     'X-RapidAPI-Key': '1f337ed6e9mshbfacca2cfdfb894p120943jsn7edec953a7e2',
+  //     'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
+  //   }
+  // };
 
-    const fetchData = async () => {
-      const response = await axios.request(options);
-      setImages(response.data.value);
-    };
+  //   const fetchData = async () => {
+  //     const response = await axios.request(options);
+  //     setImages(response.data.value);
+  //   };
 
-    useEffect(() => {
-      fetchData();
-    }, []);
+  //   useEffect(() => {
+  //     fetchData();
+  //   }, []);
 
     console.log(images)
   
