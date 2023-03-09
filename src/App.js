00a130d7aeb69a9d89f0a1a99b0b2018fce9bdc6
HollyLgru <H.grudovich@zollege.com>
2023-03-08 18:37:56 -0600

    bare bones

--- src/App.js
+++ src/App.js
@@ -3,39 +3,12 @@ import axios from 'axios';
 import  {React, useState, useEffect } from 'react';
 
 function App() {
-  const [images, setImages] = useState([])
 
-  const options = {
-    method: 'GET',
-    url: 'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI',
-    params: {q: 'ice cream', pageNumber: '1', pageSize: '2', autoCorrect: 'true'},
-    headers: {
-      'X-RapidAPI-Key': '1f337ed6e9mshbfacca2cfdfb894p120943jsn7edec953a7e2',
-      'X-RapidAPI-Host': 'contextualwebsearch-websearch-v1.p.rapidapi.com'
-    }
-  };
-
-
-    useEffect(() => {
-      const fetchData = async () => {
-        const response = await axios.request(options);
-        // console.log(response.data)
-        setImages(response.data.value);
-      };
-      fetchData();
-    }, []);
-
-    console.log(images)
   
 
   return (
     <div className="App">
-      <h1>My Images.</h1>
-      {images.map((image, index) => {
-        return (
-          <img src={image.url} alt="images" width="400" height="400"/>
-        )
-      })}
+
     </div>
   );
 }
